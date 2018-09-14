29af852a725710d10fd8cfc97a89d60ce22d6dbd
Tatiana Veratti <Tatiana.Veratti@topbox.io>
2018-09-14 15:51:03 -0500

    placeholder lorem ipsum content and browserslist

--- src/home/Home.js
+++ src/home/Home.js
@@ -2,82 +2,41 @@ import React, { Component } from 'react';
 import ipsum from 'lorem-ipsum';
 
 import { Button, PromoGrid } from 'veratti-ui';
-
-// Images
-import candles from '../assets/images/candles.jpg';
-import bowlers from '../assets/images/bowlers.jpg';
-import strike from '../assets/images/lane.jpg';
-import hands from '../assets/images/hands.jpg';
-import party from '../assets/images/party.jpg';
-import food from '../assets/images/food.jpg';
-
+import ImgPlacholder from '../assets/images/img-placeholder.png';
 import Ball from '../assets/svg/ball';
 import './Home.scss';
 
 class Home extends Component {
   render() {
+    const panels = [...Array(6)].map(_ => ({
+      title: 'Lorem Ipsum',
+      subtitle: 'Dolor sit amet.',
+      button: 'Ipsum',
+      image: ImgPlacholder
+    }));
+
     return (
       <div className="home">
         <div className="center">
           <h2>Web Design Demo</h2>
           <div className="paragraph center">
-            This site is not for a real venue. This is a technical showcase and
-            portfolio piece for
+            This site is not for a real venue. This is a technical demo by
             <Button href="https://dev.veratti.co" secondary>
               Veratti Web Development.
             </Button>
-            If you like what you see and want similar work for your business,
-            then
+            If this inspires you and your business or hobby needs a modern
+            website then
             <Button href="https://dev.veratti.co/contact" secondary>
               let's talk!
             </Button>
           </div>
           <div className="separator" />
-          <Button bg>Make Reservation</Button>
+          <Button bg>Lorem Ipsum</Button>
           <div className="separator" />
         </div>
 
         <div className="home__promo">
-          <PromoGrid
-            panels={[
-              {
-                title: 'Parties',
-                subtitle: 'Birthdays, Anniversaries, & more',
-                button: 'Plan Party',
-                image: candles
-              },
-              {
-                title: 'Leagues',
-                subtitle: 'Teams for all skill levels',
-                button: 'Register Player',
-                image: bowlers
-              },
-              {
-                title: 'Tournaments',
-                subtitle: 'Win prizes and bragging rights',
-                button: 'Register Team',
-                image: hands
-              },
-              {
-                title: 'Events',
-                subtitle: 'Special games and themes',
-                button: 'Reserve Group',
-                image: party
-              },
-              {
-                title: 'Menu',
-                subtitle: 'Tastey treats',
-                button: 'Yum Yum',
-                image: food
-              },
-              {
-                title: 'Employment',
-                subtitle: 'Now hiring',
-                button: 'See Positions',
-                image: strike
-              }
-            ]}
-          />
+          <PromoGrid panels={panels} />
         </div>
         <div className="separator" />
         <div className="home__about">
